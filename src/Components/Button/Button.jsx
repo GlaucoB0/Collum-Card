d9088f1bf5641eeb5a87f5af891ee46d32105759
GlaucoB0 <glauco.m.b.soares@gmail.com>
2024-04-15 20:49:44 -0300

    Collum Card Finalizado: alinhamento de cards, interação com o botão adicionada

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -1,7 +1,14 @@
 import './Button.css'
 const Button = ({textButton, Color}) => {
+    function mouseEnter(event) {
+        event.target.style.color = 'white'
+    }
+    function mouseLeave(event) {
+        event.target.style.color = Color
+    }
+
     return (
-        <button className="button">{textButton}</button>
+        <button className="button" onMouseEnter={mouseEnter} onMouseLeave={mouseLeave} style={{color: Color}}>{textButton}</button>
     )
 }
 export default Button
